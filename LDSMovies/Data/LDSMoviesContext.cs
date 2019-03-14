089ca43caf981c736788c1ef4324718fb0ec6962
samanthahancock <han13018@byui.edu>
2019-03-14 09:25:17 -0600

    Work with a database part

---
+++ LDSMovies/Data/LDSMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LDSMovie.Models;
+
+namespace LDSMovies.Models
+{
+    public class LDSMoviesContext : DbContext
+    {
+        public LDSMoviesContext (DbContextOptions<LDSMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LDSMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- LDSMovies/Startup.cs
+++ LDSMovies/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using LDSMovies.Models;
 
 namespace LDSMovies
 {
@@ -33,6 +35,9 @@ namespace LDSMovies
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<LDSMoviesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LDSMoviesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
